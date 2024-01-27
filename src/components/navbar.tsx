78203069e09c05ff10f7b6048e7d4af6d9d8dd80
paulodavid5 <paulodaviddias4@gmail.com>
2024-01-27 17:38:45 +0000

    First push movies routes menubar semantic-ui

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import {Menu} from "semantic-ui-react";
+export const Navbar = () => {
+  return (
+    <Menu fixed="top" size="huge">
+        <Menu.Menu position="right">
+            <Menu.Item as={Link} to="/auth">Auth</Menu.Item>
+        </Menu.Menu>
+    </Menu>
+  )
+}
